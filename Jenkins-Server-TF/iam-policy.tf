4b8d55bb3abfd3fbfd01d5705a48caec0a77f38f
Mohamedzaakii <mohamedzaki827@gmail.com>
2026-05-06 05:48:18 +0300

    change the ec2 instance type to m7i-flex.large

--- Jenkins-Server-TF/iam-policy.tf
+++ Jenkins-Server-TF/iam-policy.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_role_policy_attachment" "iam-policy" {
-  role = aws_iam_role.iam-role.name
+  role       = aws_iam_role.iam-role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
